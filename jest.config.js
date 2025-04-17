2a61b87a2c5c2c503592270fb82660f76f21356d
Rodger Blom <rodger.blom@truelime.nl>
2025-04-17 08:32:24 +0200

    chore: update dependencies and add test script
    - Added test script to package.json

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   moduleDirectories: ["node_modules"],
+  testMatch: ["**/node-zerox/tests/**/*.test.ts", "**/node-zerox/tests/**/*.spec.ts"],
   transform: {
     "^.+\\.tsx?$": [
       "ts-jest",
